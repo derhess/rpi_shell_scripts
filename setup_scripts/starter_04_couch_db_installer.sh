be1aff1ce1f1d5ec943fb667451cb13c04b02dd0
derhess <florianweil@gmx.de>
2017-12-19 17:56:24 +0100

    Added CouchDB 2.x installation script

--- setup_scripts/starter_04_couch_db_installer.sh
+++ setup_scripts/starter_04_couch_db_installer.sh
@@ -4,7 +4,8 @@ COUCH_DB_DOWNLOAD_FOLDER_TARGET_PATH="/home/pi"
 COUCH_DB_FOLDER="/home/couchdb"
 COUCH_DB_DOWNLOAD_MIRROR="http://apache.mirror.digionline.de/couchdb/source/2.1.1/apache-couchdb-2.1.1.tar.gz"
 COUCH_DB_LOCAL_INI_FILE_PATH="/home/pi/rpi_shell_scripts/other_configs/couchdb/local.ini"
-COUCH_DB_DAEMON_SYSTEMD_SERVICE_FILE_PATH= "/home/pi/rpi_shell_scripts/systemd_scripts/couchdb.service"
+COUCH_DB_DAEMON_SYSTEMD_SERVICE_FILE_PATH="/home/pi/rpi_shell_scripts/systemd_scripts/couchdb.service"
+COUCH_DB_DAEMON_SYSTEMD_SERVICE_TARGET="/etc/systemd/system/couchdb.service"
 
 echo "Installing CouchDB"
 cd $COUCH_DB_DOWNLOAD_FOLDER_TARGET_PATH
@@ -31,6 +32,7 @@ sudo chown couchdb:couchdb $COUCH_DB_FOLDER
 
 # download source code
 echo "Downloach CouchDB Sources for Version 2.x"
+cd $COUCH_DB_DOWNLOAD_FOLDER_TARGET_PATH"/"$COUCH_DB_DOWNLOAD_FOLDER_NAME
 wget $COUCH_DB_DOWNLOAD_MIRROR
 tar zxvf apache-couchdb-2.*.tar.gz
 cd apache-couchdb-2.*/
@@ -46,6 +48,7 @@ echo "Your CouchDB should compiled successfully"
 echo "Next step is the configuration on your system"
 cd ./rel/couchdb
 pwd
+echo "Copy the CouchDB directory to the folder:"$COUCH_DB_FOLDER
 sudo cp -Rp * $COUCH_DB_FOLDER
 sudo chown -R couchdb:couchdb $COUCH_DB_FOLDER
 
@@ -54,15 +57,14 @@ echo "copy new local.ini config file to couchdb - just changed the chttp local b
 sudo mv $COUCH_DB_FOLDER"/etc/local.ini" $COUCH_DB_FOLDER"/etc/local.ini.orig"
 sudo cp $COUCH_DB_LOCAL_INI_FILE_PATH $COUCH_DB_FOLDER"/etc/"
 
-
 #clean installation folder
 echo "clean some the installation folders"
-sudo rm -rf $COUCH_DB_FOLDER_TARGET_PATH"/"$COUCH_DB_DOWNLOAD_FOLDER_NAME"/"
+sudo rm -rf $COUCH_DB_DOWNLOAD_FOLDER_TARGET_PATH"/"$COUCH_DB_DOWNLOAD_FOLDER_NAME"/"
 
 
 #Create, Register and Install a CouchDB Starter service after system reboot
 echo "Create a systemd service for CouchDB"
-sudo cp -u $COUCH_DB_DAEMON_SYSTEMD_SERVICE_FILE_PATH /etc/systemd/system/couchdb.service
+sudo cp $COUCH_DB_DAEMON_SYSTEMD_SERVICE_FILE_PATH $COUCH_DB_DAEMON_SYSTEMD_SERVICE_TARGET
 
 #Register your systemd service
 sudo systemctl daemon-reload
